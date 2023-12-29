70eb41b1f535efc4139dcdff4ace37b379791a38
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2023-12-29 22:34:32 +0500

    Add project files.

---
+++ Repetition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Channels;
+
+int MyArray = int.Parse(Console.ReadLine());
+int [] array =[12, 13, 14, 5, 56 ];
+foreach (int i in array) 
+{
+    if (i == MyArray)
+    {
+        Console.WriteLine(i);
+    }
+    else
+    { 
+        Console.WriteLine("false " + i); 
+    }
+}
+for (int i = 0; i < array.Length; i++) 
+{
+    if (array[i] == MyArray)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    else 
+    {
+        array[i] *= 2;
+    }
+}
