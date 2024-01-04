0d99cd7af754b34a7bcb60bb807bbf8ee2971179
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2024-01-04 08:46:27 +0500

    info home work  03.01.24

---
+++ home work/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace home_work
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+            try
+            {
+               
+                Console.Write("Enter the directory path: ");
+                string directoryPath = Console.ReadLine()!;
+
+               
+                string[] filePaths = FindFiles(directoryPath);
+
+               
+                if (filePaths.Length > 0)
+                {
+                    Console.WriteLine($"Found files:");
+                    foreach (var filePath in filePaths)
+                    {
+                       
+                        Console.WriteLine(filePath);
+                    }
+                }
+                else
+                {
+                    
+                    Console.WriteLine("No files found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static string[] FindFiles(string directoryPath)
+        {
+            
+            string[] files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+
+            return files;
+        }
+    }
+}
