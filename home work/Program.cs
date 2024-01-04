a106531b1a39709fd5006c2442c42167cc415ffa
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2024-01-04 14:33:53 +0500

    info: jisone projeck

--- home work/Program.cs
+++ home work/Program.cs
@@ -1,3 +1,39 @@
-﻿using home_work;
+﻿void Qidiruv(List<string> topilganmalumot, string Path, string FaylNomi)
+{
+    string[] Fayllar = Directory.GetFiles(Path);
+    foreach (string f in Fayllar)
+    {
+        string[] filenomi = f.Split('\\');
+        if (filenomi[filenomi.Length - 1] == FaylNomi)
+            topilganmalumot.Add(f);
+    }
 
-Class1 searchFile = new Class1();
+   
+    string[] folders = Directory.GetDirectories(Path);
+    foreach (string f in folders)
+    {
+       
+        try
+        {
+            Qidiruv(topilganmalumot, f, FaylNomi);
+        }
+        catch { }
+    }
+
+}
+
+
+Console.Write("Fayl nomini kiritng: ");
+string FaylNomi = Console.ReadLine();
+Console.WriteLine("......");
+
+List<string> Topilmalar = new List<string>();
+DriveInfo[] Disklar = DriveInfo.GetDrives();
+for (int i = 0; i < Disklar.Length; i++)
+{
+    Qidiruv(Topilmalar, Convert.ToString(Disklar[i]), FaylNomi);
+}
+
+Console.WriteLine("resoult: ");
+foreach (string f in Topilmalar)
+    Console.WriteLine(f);
